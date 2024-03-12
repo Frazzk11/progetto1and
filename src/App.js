5c98815034dabc18f1a1c74f194f023c1b6d85d1
Francesco <ambroesandreotti@gmail.com>
2024-03-12 11:00:58 +0100

    Homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './common/header/Header'
 import Footer from './common/Footer/Footer';
+import Home from './pages/Home/Home'
 
 function App() {
   return (
     <div className="App">
 
       <Header />
+      <Home />
       <Footer />
     </div>
   );
